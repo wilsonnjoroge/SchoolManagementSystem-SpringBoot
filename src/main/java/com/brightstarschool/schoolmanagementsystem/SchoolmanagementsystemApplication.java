210f50ecaf7310d78b005442cf04ba9ad82503bd
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-07-13 13:05:30 +0300

    V1

---
+++ src/main/java/com/brightstarschool/schoolmanagementsystem/SchoolmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.brightstarschool.schoolmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolmanagementsystemApplication.class, args);
+	}
+
+}
