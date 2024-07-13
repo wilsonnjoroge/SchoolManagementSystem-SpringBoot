927613914a2b39a923ebc6f1b8127198561f3518
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-07-13 16:00:28 +0300

    Added endpoint for getting all students

--- src/main/java/com/brightstarschool/schoolmanagementsystem/controller/StudentController.java
+++ src/main/java/com/brightstarschool/schoolmanagementsystem/controller/StudentController.java
@@ -1,24 +1,36 @@
 package com.brightstarschool.schoolmanagementsystem.controller;
 
+import com.brightstarschool.schoolmanagementsystem.dto.StudentDTO;
 import com.brightstarschool.schoolmanagementsystem.dto.StudentSaveDTO;
 import com.brightstarschool.schoolmanagementsystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin
-@RequestMapping("api/v1/student")
+@RequestMapping("api/v1/students")
 public class StudentController {
 
     @Autowired
     private StudentService studentService;
 
 
-    @PostMapping("/add-students")
+    @PostMapping(path = "/add-students")
     public String saveStudent(@RequestBody StudentSaveDTO studentSaveDTO)
     {
-        String stname = studentService.addStudent(studentSaveDTO);
-        return stname;
+        String studentName = studentService.addStudent(studentSaveDTO);
+        return studentName;
+    };
+
+    @GetMapping(path = "/view-all-students")
+    public List<StudentDTO> getAllStudents()
+    {
+        List<StudentDTO> allStudents = studentService.getAllStudents();
+
+        return allStudents;
+
     };
 
 }
--- src/main/java/com/brightstarschool/schoolmanagementsystem/service/implementation/StudentServiceImplementation.java
+++ src/main/java/com/brightstarschool/schoolmanagementsystem/service/implementation/StudentServiceImplementation.java
@@ -1,11 +1,15 @@
 package com.brightstarschool.schoolmanagementsystem.service.implementation;
 
+import com.brightstarschool.schoolmanagementsystem.dto.StudentDTO;
 import com.brightstarschool.schoolmanagementsystem.dto.StudentSaveDTO;
 import com.brightstarschool.schoolmanagementsystem.entity.Student;
 import com.brightstarschool.schoolmanagementsystem.repository.StudentRepository;
 import com.brightstarschool.schoolmanagementsystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class StudentServiceImplementation implements StudentService {
@@ -28,5 +32,25 @@ public class StudentServiceImplementation implements StudentService {
 
         return student.getStudentName();
 
+    }
+
+    @Override
+    public List<StudentDTO> getAllStudents() {
+        List<Student> getStudent = studentRepository.findAll();
+        List<StudentDTO> studentDTOList = new ArrayList<>();
+
+        for(Student student : getStudent)
+        {
+            StudentDTO studentDTO = new StudentDTO
+                    (
+                            student.getStudentId(),
+                            student.getStudentName(),
+                            student.getAdress(),
+                            student.getPhoneNumber()
+                    );
+            studentDTOList.add(studentDTO);
+        }
+
+        return studentDTOList;
     }
 }
