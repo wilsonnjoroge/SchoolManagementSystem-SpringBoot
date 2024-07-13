799c2160565fff5788ea7ab054faef2343f3c8ff
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-07-13 15:20:43 +0300

    Registered a user successfully

--- src/main/java/com/brightstarschool/schoolmanagementsystem/controller/StudentController.java
+++ src/main/java/com/brightstarschool/schoolmanagementsystem/controller/StudentController.java
@@ -1,15 +1,24 @@
 package com.brightstarschool.schoolmanagementsystem.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.brightstarschool.schoolmanagementsystem.dto.StudentSaveDTO;
+import com.brightstarschool.schoolmanagementsystem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping("api/v1/student")
 public class StudentController {
 
+    @Autowired
+    private StudentService studentService;
 
 
+    @PostMapping("/add-students")
+    public String saveStudent(@RequestBody StudentSaveDTO studentSaveDTO)
+    {
+        String stname = studentService.addStudent(studentSaveDTO);
+        return stname;
+    };
 
 }
--- src/main/java/com/brightstarschool/schoolmanagementsystem/repository/StudentRepository.java
+++ src/main/java/com/brightstarschool/schoolmanagementsystem/repository/StudentRepository.java
@@ -1,4 +1,7 @@
 package com.brightstarschool.schoolmanagementsystem.repository;
 
-public interface StudentRepository {
+import com.brightstarschool.schoolmanagementsystem.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
 }
