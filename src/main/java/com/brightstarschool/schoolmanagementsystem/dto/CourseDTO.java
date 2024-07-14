38aee46e66f4247121c5537a2f56317cd2601b46
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-07-14 20:32:56 +0300

    Added CRUD endpoints for courses

---
+++ src/main/java/com/brightstarschool/schoolmanagementsystem/dto/CourseDTO.java
@@ -0,0 +1,2 @@
+package com.brightstarschool.schoolmanagementsystem.dto;public class CourseDTO {
+}
---
+++ src/main/java/com/brightstarschool/schoolmanagementsystem/service/implementation/CourseServiceImplementation.java
@@ -0,0 +1,2 @@
+package com.brightstarschool.schoolmanagementsystem.service.implementation;public class CourseServiceImplementation {
+}
