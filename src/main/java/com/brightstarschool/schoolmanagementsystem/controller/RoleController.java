f02076af35045994a5af399d21d7aa1f67aa8a18
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-07-20 11:51:35 +0300

    Added api endpoint for getting all the roles in the db

--- src/main/java/com/brightstarschool/schoolmanagementsystem/controller/RoleController.java
+++ src/main/java/com/brightstarschool/schoolmanagementsystem/controller/RoleController.java
@@ -1,11 +1,15 @@
 package com.brightstarschool.schoolmanagementsystem.controller;
 
 
+import com.brightstarschool.schoolmanagementsystem.dto.RoleDTO;
 import com.brightstarschool.schoolmanagementsystem.dto.RoleSaveDTO;
+import com.brightstarschool.schoolmanagementsystem.entity.Role;
 import com.brightstarschool.schoolmanagementsystem.service.interfaces.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -18,5 +22,11 @@ public class RoleController {
     public ResponseEntity<String> createRole(@RequestBody RoleSaveDTO roleSaveDTO) {
         String response = roleService.addRole(roleSaveDTO);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping(path = "/get-all-roles")
+    public ResponseEntity<List<RoleDTO>> getAllRoles() {
+        List<RoleDTO> roles = roleService.getAllRoles();
+        return ResponseEntity.ok(roles);
     }
 }
