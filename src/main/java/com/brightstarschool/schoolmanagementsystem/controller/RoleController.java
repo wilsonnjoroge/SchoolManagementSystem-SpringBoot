90dfa661e725007dae4230439b4b8cfba81412d9
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-07-22 08:21:32 +0300

    Added swagger configs

--- src/main/java/com/brightstarschool/schoolmanagementsystem/controller/RoleController.java
+++ src/main/java/com/brightstarschool/schoolmanagementsystem/controller/RoleController.java
@@ -4,8 +4,9 @@ package com.brightstarschool.schoolmanagementsystem.controller;
 import com.brightstarschool.schoolmanagementsystem.dto.RoleDTO;
 import com.brightstarschool.schoolmanagementsystem.dto.RoleSaveDTO;
 import com.brightstarschool.schoolmanagementsystem.dto.RoleUpdateDTO;
-import com.brightstarschool.schoolmanagementsystem.entity.Role;
 import com.brightstarschool.schoolmanagementsystem.service.interfaces.RoleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import java.util.List;
 @RestController
 @CrossOrigin
 @RequestMapping(path = "api/v1/roles")
+@Api(value = "Role Management System", description = "Operations pertaining to roles in Role Management System")
 public class RoleController {
     private RoleService roleService;
 
@@ -24,18 +26,21 @@ public class RoleController {
     }
 
     @PostMapping(path = "/add-role")
+    @ApiOperation(value = "Create a new role")
     public ResponseEntity<String> createRole(@RequestBody RoleSaveDTO roleSaveDTO) {
         String response = roleService.addRole(roleSaveDTO);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping(path = "/get-all-roles")
+    @ApiOperation(value = "Get all roles")
     public ResponseEntity<List<RoleDTO>> getAllRoles() {
         List<RoleDTO> roles = roleService.getAllRoles();
         return ResponseEntity.ok(roles);
     }
 
     @PutMapping(path = "/update-role/{id}")
+    @ApiOperation(value = "Update an existing role")
     public ResponseEntity<String> updateRole(@PathVariable long id, @RequestBody RoleUpdateDTO roleUpdateDTO)
     {
         String response = roleService.updateRole(id, roleUpdateDTO);
@@ -43,6 +48,7 @@ public class RoleController {
     }
 
     @DeleteMapping(path = "/delete-role/{id}")
+    @ApiOperation(value = "Delete a role")
     public String deleteRole(@PathVariable("id") long id) {
         boolean deleteRole = roleService.deleteRole(id);
         if (deleteRole) {
