f6809667825561f926856d5c06fd825814256df9
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-08-03 08:07:32 +0300

    Completed dorm endpoints and assigned the student dorm, working on Subject Management

--- src/main/java/com/brightstarschool/schoolmanagementsystem/repository/DormitoryRepository.java
+++ src/main/java/com/brightstarschool/schoolmanagementsystem/repository/DormitoryRepository.java
@@ -3,5 +3,10 @@ package com.brightstarschool.schoolmanagementsystem.repository;
 import com.brightstarschool.schoolmanagementsystem.entity.Dormitory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DormitoryRepository extends JpaRepository<Dormitory, Long> {
+    Optional<Dormitory> findByDormCode(long dormCode);
+
+    Optional<Dormitory> findByDormName(String dormName);
 }
---
+++ src/main/java/com/brightstarschool/schoolmanagementsystem/repository/SubjectRepository.java
@@ -0,0 +1,13 @@
+package com.brightstarschool.schoolmanagementsystem.repository;
+
+import com.brightstarschool.schoolmanagementsystem.entity.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.Optional;
+
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
+    Optional<Subject> findBySubjectCode(long subjectCode);
+
+    Optional<Subject> findBySubjectName(String subjectName);
+}
