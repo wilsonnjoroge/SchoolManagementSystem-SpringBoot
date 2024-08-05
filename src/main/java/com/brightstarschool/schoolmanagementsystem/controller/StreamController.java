d4250e0e29a8d3660fbb3631a0bda4db9860839c
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-08-05 09:55:53 +0300

    Added stream controllers

--- src/main/java/com/brightstarschool/schoolmanagementsystem/controller/StreamController.java
+++ src/main/java/com/brightstarschool/schoolmanagementsystem/controller/StreamController.java
@@ -1,2 +1,27 @@
-package com.brightstarschool.schoolmanagementsystem.controller;public class StreamController {
+package com.brightstarschool.schoolmanagementsystem.controller;
+
+import com.brightstarschool.schoolmanagementsystem.dto.StreamSaveDTO;
+import com.brightstarschool.schoolmanagementsystem.service.implementation.StreamServiceImplementation;
+import com.brightstarschool.schoolmanagementsystem.service.interfaces.StreamService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping(path = "api/v1/streams")
+public class StreamController {
+
+    private StreamServiceImplementation streamServiceImplementation;
+
+    public StreamController(StreamServiceImplementation streamServiceImplementation) {
+        this.streamServiceImplementation = streamServiceImplementation;
+    }
+
+    @PostMapping(path = "/add-stream")
+    public ResponseEntity<String> addStream(@RequestBody StreamSaveDTO streamSaveDTO)
+    {
+        String response = streamServiceImplementation.addStream(streamSaveDTO);
+
+        return ResponseEntity.ok(response);
+    }
 }
